32c701a8058de41fa31d2f0b0e11a5475d1244d8
M.Minhaj <minhajmuhammad265@gmail.com>
2025-09-15 09:52:14 +0500

    Add files via upload

---
+++ PIAIC-Assignment2.py
@@ -0,0 +1,36 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error! Division by zero."
+
+print("\nCalculator")
+print("Choose operation: +, -, *, /\n")
+while True:
+    operation = input("Enter operator (+, -, *, /) or 'exit' to quit: ")
+    if operation.lower() == 'exit':
+        print("Calculator closed. Goodbye!")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if operation == '+':
+        print("Result:", add(num1, num2))
+    elif operation == '-':
+        print("Result:", subtract(num1, num2))
+    elif operation == '*':
+        print("Result:", multiply(num1, num2))
+    elif operation == '/':
+        print("Result:", divide(num1, num2))
+    else:
+        print("Invalid operator")
